3072c0b4bb2cd56ba6c90eb06df4079461d5ad0b
javier <francisco.rodriguez@magnothecnology.com>
2022-06-05 04:52:12 -0500

    Creando primeras pruebas con jest, funcionando

---
+++ tests/unit/components/counter.spec.js
@@ -0,0 +1,25 @@
+import {shallowMount} from '@vue/test-utils'
+import Counter from '@/components/Counter'
+
+
+describe('Counter Component', () =>{
+
+     test('Debe de hacer match con el snapshot', () => {
+
+         const wrapper = shallowMount(Counter)
+
+         expect(wrapper.html()).toMatchSnapshot()
+     })
+
+
+    test('Debe de tener el valor por defecto "Counter" ', () => { 
+        
+        const wrapper = shallowMount(Counter)
+        expect(wrapper.find('h2').exists()).toBeTruthy()
+
+        const h2Value = wrapper.find('h2').text()
+
+        expect(h2Value).toBe('Counter')
+    })
+
+})
